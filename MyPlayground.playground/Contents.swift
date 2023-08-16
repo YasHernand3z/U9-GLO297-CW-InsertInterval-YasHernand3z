0b8fc228f1009511465f5f4870e5fa39996de703
YasHernand3z <yasmine.hernandez@globant.com>
2023-08-16 17:21:25 -0600

    Add: Add case where new interval overrides first element of the array

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,17 +3,22 @@ func mergeIntervals(intervals: [(Int, Int)], newInterval: (Int, Int)) -> [(Int, 
         return [newInterval]
     }
     
-    var indexStart = intervals.lastIndex(where: {return newInterval.0 > $0.0}) ?? -1
-    var indexEnd = intervals.lastIndex(where: {return newInterval.1 > $0.0}) ?? -1
+    var indexStart = intervals.lastIndex(where: {return newInterval.0 > $0.0}) ?? 0
+    var indexEnd = intervals.lastIndex(where: {return newInterval.1 > $0.0}) ?? 0
     var newIntervals: [(Int, Int)] = []
     
-    if indexStart >= 0 {
+    print("\(indexStart) \(indexStart)")
+    
+    if indexStart > 0 {
         newIntervals.append(contentsOf: intervals[0..<indexStart])
-        let value0 = intervals[indexStart].0 < newInterval.0 ? intervals[indexStart].0 : newInterval.0
-        let value1 = intervals[indexEnd].1 > newInterval.1 ? intervals[indexEnd].1 : newInterval.1
-        newIntervals.append((value0, value1))
+        newIntervals.append(updateInterval(startInterval: intervals[indexStart], endInterval: intervals[indexEnd], newInterval: newInterval))
     } else {
-        newIntervals.append(newInterval)
+        if isOverlaping(intervalA: newInterval, intervalB: intervals[indexStart]) {
+            newIntervals.append(updateInterval(startInterval: intervals[indexStart], endInterval: intervals[indexEnd], newInterval: newInterval))
+        } else {
+            newIntervals.append(newInterval)
+            newIntervals.append(intervals[indexStart])
+        }
     }
     
     let index =  indexEnd + 1
@@ -29,6 +34,18 @@ func mergeIntervals(intervals: [(Int, Int)], newInterval: (Int, Int)) -> [(Int, 
     return newIntervals
 }
 
+func isOverlaping(intervalA: (Int, Int), intervalB: (Int, Int)) -> Bool {
+    return  intervalA.1 >= intervalB.0 || intervalA.1 >= intervalB.1
+}
+
+func updateInterval(startInterval: (Int, Int), endInterval: (Int, Int), newInterval: (Int, Int)) -> (Int, Int)
+{
+    let startRange = startInterval.0 < newInterval.0 ? startInterval.0 : newInterval.0
+    let endRange = endInterval.1 > newInterval.1 ? endInterval.1 : newInterval.1
+    
+    return (startRange, endRange)
+}
+
 
 let newIntervals01 = mergeIntervals(intervals: [(1,3), (6,9)], newInterval: (2,8))
 let newIntervals02 = mergeIntervals(intervals: [], newInterval: (2,8))
@@ -36,3 +53,4 @@ let newIntervals03 = mergeIntervals(intervals: [(1,3), (6,9)], newInterval: (2,5
 let newIntervals04 = mergeIntervals(intervals: [(1,2), (3,5), (6,7), (8,10), (12, 16)], newInterval: (4,9))
 let newIntervals05 = mergeIntervals(intervals: [(1,3), (6,9), (12,16)], newInterval: (2,5))
 let newIntervals06 = mergeIntervals(intervals: [(6,9), (12,16)], newInterval: (1,3))
+let newIntervals07 = mergeIntervals(intervals: [(3,4), (5,7)], newInterval: (2,3))
